8081fce0e813eea3cb264e1bb09b34ddd2b97d6e
Kundai <kundaimuchemwa456@gmail.com>
2023-09-08 23:08:11 -0700

    hie

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,2 @@
 #!/bin/bash
-#include <stdio.h>
-/**
- *main - Entry data
- *Description: print "\"Programming is like building a multilingual puzzle"
- *Return:0
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+halla
