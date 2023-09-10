9117dbdd905a9e3fc04e89c0c93fdd28046399d7
Kundai <kundaimuchemwa456@gmail.com>
2023-09-10 06:27:31 -0700

    if

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
