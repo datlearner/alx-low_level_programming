c27d17fc39418e1495c0b0ee352aaae4da8d74f9
Kundai <kundaimuchemwa456@gmail.com>
2023-09-10 08:23:20 -0700

    g

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,22 +5,19 @@
  */
 int main(void)
 {
-int n, m, l;
-for (n = 1; n < 89; n++)
+int n, m;
+for (n = 48; n <= 56; n++)
 {
-for (m = 49; m < 58; m++)
+for (m = 49; m <= 57; m++)
 {
-for (l = 50; l < 58; l++)
-{
-if (l > m && m > n)
+if (m > n)
 {
 putchar(n);
 putchar(m);
-putchar(l);
-if (n != 55 || m != 56)
+if (n != 56 || m != 57)
 {
 putchar(',');
-}
+putchar(' ');
 }
 }
 }
