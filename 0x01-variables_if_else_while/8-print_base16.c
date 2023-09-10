f0029e2f1e627b8cf99ea7f26e7ffb3bd28e71d1
Kundai <kundaimuchemwa456@gmail.com>
2023-09-10 07:16:24 -0700

    h

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int n;
+char ch;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
