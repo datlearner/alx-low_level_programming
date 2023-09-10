0f606c60db9d87aa0d5fc3abc5556e7808d333f4
Kundai <kundaimuchemwa456@gmail.com>
2023-09-10 06:08:16 -0700

    hie

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - function
  * Return: 0 (Success)
  */
 int main(void)
 {
-printf("hie");
+int n, last;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last = n % 10;
+if (last > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, last);
+else if (last == 0)
+printf("Last digit of %d is %d and is 0\n", n, last);
+else if (last < 6 && last != 0)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 return (0);
 }
