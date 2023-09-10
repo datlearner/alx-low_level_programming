970df0d9bd39a157b1d22809f1435d1851e3ef3c
Kundai <kundaimuchemwa456@gmail.com>
2023-09-10 06:16:32 -0700

    hie

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+printf("%c ", letter);
+letter++;
+}
+printf("\n");
+return (0);
+}
