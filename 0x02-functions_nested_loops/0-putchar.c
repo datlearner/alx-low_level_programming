8449ce884f6e99ec92dd2d5cb8b1051034d48b99
Kundai <kundaimuchemwa456@gmail.com>
2023-09-12 06:45:12 -0700

    h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - writes the function
+ * main - writes _putchar
  * Return: 0 (Success)
  */
 int main(void)
 {
-printf("hie");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
