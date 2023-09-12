e2d5e7ae7c8dc0ebc687b97e0487626388ed8999
Kundai <kundaimuchemwa456@gmail.com>
2023-09-12 14:44:41 -0700

    s

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
