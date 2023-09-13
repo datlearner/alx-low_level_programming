4e58e53131e98a10f791394b6a6af1e809831547
Kundai <kundaimuchemwa456@gmail.com>
2023-09-12 19:14:53 -0700

    m

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.c"
+/**
+ * _abs - function find the absolute value of a number
+ * @a: function parameter
+ * Return: -a or a;
+ */
+int _abs(int a)
+{
+if (a < 0)
+return (-a);
+else if (a >= 0)
+{
+return (a);
+}
+}
