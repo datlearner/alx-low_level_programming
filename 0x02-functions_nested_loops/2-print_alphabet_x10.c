90dce1a9c3c83cfdc9fc113559999a7b2f1f916c
Kundai <kundaimuchemwa456@gmail.com>
2023-09-12 18:19:25 -0700

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  print_alphabet_x10 - print alphabet
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+for (j = 'a'; j <= 'z'; j++)
+_putchar(j);
+_putchar('\n');
+}
+}
