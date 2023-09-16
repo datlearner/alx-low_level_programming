92f37099a20241342eeccdff4385528408342f7c
Kundai <kundaimuchemwa456@gmail.com>
2023-09-15 18:14:11 -0700

    m

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,5 +5,9 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 int _isupper(int c);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
 
 #endif
