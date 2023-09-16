0d4c74ab27644642b3c078d09a9c4c0b07952ffe
Kundai <kundaimuchemwa456@gmail.com>
2023-09-15 17:34:25 -0700

    m

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _issuper - check for uppercase character
+ * @c: function parameter
+ * Return: 1 (success) 0 (failure)
+ */
+
+int _issuper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
