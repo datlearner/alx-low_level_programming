6f81d4ca0a5babd2ffcf7eb135d3217934799193
Kundai <kundaimuchemwa456@gmail.com>
2023-09-15 18:59:41 -0700

    m

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - print staright line on the terminal
+ * @n: numb of times e char '_' will be printed
+ * Return: void
+ */
+void print_line(int n)
+{
+if (n <= 0)
+_putchar('n');
+else
+{
+int i;
+for (i = 1; i <= n; i++)
+_putchar('_');
+_putchar('\n');
+}
+}
