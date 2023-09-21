f7be76e8906b7abcc3c96146cb84fba073f303fd
Kundai <kundaimuchemwa456@gmail.com>
2023-09-20 22:30:32 -0700

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,11 +4,18 @@
  * _strcat - concatenat
  * @dest: concatenates
  * @src: string
- * Return: none
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
-printf("hie%s", dest);
-printf("hie%s", src);
-return (0);
+int r;
+int b = 0;
+while (dest[b] != '\0')
+{
+b++;
+}
+for (r = 0 ; r < b && src[r] != '\0' ; r++)
+dest[b + r] = src[r];
+dest[b + r] = '\0';
+return (dest);
 }
