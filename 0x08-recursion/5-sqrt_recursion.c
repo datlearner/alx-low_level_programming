bcdde7c7697c588c6b596951c0fb8e70a735d1e7
Kundai <kundaimuchemwa456@gmail.com>
2023-09-28 18:55:22 -0700

    m

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,20 +3,37 @@
 /**
  * _sqrt_recursion - square root of Number
  * @n: does not have a natural number
+ * @digit: digit
  * Return: -1
  */
+int sqrr(int n, int digit);
 int _sqrt_recursion(int n)
 {
 if (n < 0)
 {
 return (-1);
 }
-else if (n == 0 && n == 1)
+else if (n == 0 || n == 1)
 {
 return (n);
 }
 else
 {
-return (n * _sqrt_recursion(n - 1));
+return (sqrr(n, n / 2));
 }
+}
+/**
+ * sqrr - calc sqr
+ * @n: digit
+ * @digit: digit number
+ * Return: sqr
+ */
+int sqrr(int n, int digit)
+{
+int new_digit = (digit + n / digit) / 2;
+if (new_digit == digit || new_digit + 1 == digit)
+{
+return (digit);
+}
+return (sqrr(n, new_digit));
 }
