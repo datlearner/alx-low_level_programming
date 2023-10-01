575217037637c6c071d08dd18d62a59803a2f4fe
Kundai <kundaimuchemwa456@gmail.com>
2023-10-01 07:17:47 -0700

    m

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,31 +6,13 @@
  * @s2: string
  * Return: one otherwise zero
  */
-int wild_Cmp(char *s1, char *s2);
 int wildcmp(char *s1, char *s2)
 {
-return (wild_Cmp(*s1, *s2));
-}
-/**
- * wild_Cmp - condition comparison
- * @s1: string
- * @s2: string
- * Return: condition
- */
-int wild_Cmp(char *s1, char *s2)
-{
-if (*s == '\0' && *s2 == '\0')
-return (1);
+if (*s2 == '\0')
+return ((*s1 == '\0'));
+if (*s1 == *s2 || *s2 == '?')
+return (wildcmp(s1 + 1, s2 + 1));
 if (*s2 == '*')
-{
-if (*(*s2 + 1) == '\0')
-return (1);
-else if (*s1 == '\0')
-return (0);
-else
-return (wild_Cmp(*s1 + *s2 + 1) || wild_Cmp(*s1 + 1, *s2));
-}
-if (s == *s2 || *s2 == '?')
-return (wild_Cmp(*s1 + 1, *s2 + 1));
+return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 return (0);
 }
