eafa6bd178e48ff8705761eeee370611b60a89c5
Kundai <kundaimuchemwa456@gmail.com>
2023-10-03 06:25:25 -0700

    m

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: (success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
