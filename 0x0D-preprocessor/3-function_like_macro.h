b51683bc82921c482062eac884befa5c59f45ad4
Kundai <kundaimuchemwa456@gmail.com>
2023-10-10 03:41:02 -0700

    m

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-#define ABS(num) ((num) > (-1) ? (num) : (num * -1))
+#ifndef _FUNCTION_LIKE_MACRO
+#define _FUNCTION_LIKE_MACRO
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 #endif
