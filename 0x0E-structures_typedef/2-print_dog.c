000ce35c376266cdc5756876935dd1ddeb3bc4cf
Kundai <kundaimuchemwa456@gmail.com>
2023-10-10 06:03:11 -0700

    m

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,13 +7,16 @@
  */
 void print_dog(struct dog *d)
 {
-if (!d)
+if (d)
 {
-printf("no infor provided.\n");
-return;
+if (!(d->name))
+printf("Name: (nil)\n");
+else
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+if (!(d->owner))
+printf("Owner: (nil)\n");
+else
+printf("Owner: %s\n", d->owner);
 }
-printf("Name: %s\nAge: %f\nOwner: %s\n",
-(d->name) ? d->name : "(nil)",
-d->age,
-(d->owner) ? d->owner : "(nil)");
 }
