595e11f82ef9a5663ed068d5f47c324631faa506
Kundai <kundaimuchemwa456@gmail.com>
2023-10-19 04:34:25 -0700

    list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,27 +9,20 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-        list_t *new_node = NULL;
-
-        if (!head || !str)
-                return (NULL);
-
-        new_node = malloc(sizeof(list_t));
-
-        if (!new_node)
-                return (NULL);
-
-        new_node->str = strdup(str);
-
-        if (!new_node->str)
-        {
-                free(new_node);
-                return (NULL);
-        }
-
-        new_node->len = strlen(str);
-        new_node->next = *head;
-        *head = new_node;
-
-        return (new_node);
+list_t *new_node = NULL;
+if (!head || !str)
+return (NULL);
+new_node = malloc(sizeof(list_t));
+if (!new_node)
+return (NULL);
+new_node->str = strdup(str);
+if (!new_node->str)
+{
+free(new_node);
+return (NULL);
+}
+new_node->len = strlen(str);
+new_node->next = *head;
+*head = new_node;
+return (new_node);
 }
