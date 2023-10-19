77abb9b32a4e540ef6dca6b75caab45d8894a8c1
Kundai <kundaimuchemwa456@gmail.com>
2023-10-19 04:30:42 -0700

    list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,26 +1,35 @@
+#include "lists.h"
+
 /**
- * add_node - adds a new node
- * @head: pointer to a pointer
- * @str: the string addition
- *
+ * add_node - node
+ * @head: head
+ * @str: string
  * Return: address
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *nod = NULL;
-if (!head || !str)
-return (NULL);
-nod = malloc(sizeof(list_t));
-if (!nod)
-return (NULL);
-nod->str = strdup(str);
-if (!nod->str)
-{
-free(nod);
-return (NULL);
-}
-nod->len = strlen(str);
-nod->next = *head;
-*head = nod;
-return (nod);
+        list_t *new_node = NULL;
+
+        if (!head || !str)
+                return (NULL);
+
+        new_node = malloc(sizeof(list_t));
+
+        if (!new_node)
+                return (NULL);
+
+        new_node->str = strdup(str);
+
+        if (!new_node->str)
+        {
+                free(new_node);
+                return (NULL);
+        }
+
+        new_node->len = strlen(str);
+        new_node->next = *head;
+        *head = new_node;
+
+        return (new_node);
 }
