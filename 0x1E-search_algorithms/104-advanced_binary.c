badf83aa69e71206d4b03113d46d1114046c180a
datlearner <kundaimuchemwa456@gmail.com>
2024-04-09 00:52:32 -0700

    advanced task

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,36 +1,48 @@
 #include "search_algos.h"
+
 /**
- * advanced_binary - function that searches for
- * a value in a sorted array of integers.
- * @array : pointer to the first element of the array to search in.
- * @size : the number of elements in array.
- * @value : the value to search for.
- * Return: index of value or -1 if not found.
-*/
+  * advanced_binary_recursive - Searches recursively
+  * @array: [sub]array to search.
+  * @left: The starting index of the [sub]array to search.
+  * @right: The ending index of the [sub]array to search.
+  * @value: The value
+  *
+  * Return: If the value is not present, -1.
+  * Description: Prints the [sub]array being searched after each change.
+  */
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
+{
+	size_t i;
+
+	if (right < left)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+
+	i = left + (right - left) / 2;
+	if (array[i] == value && (i == left || array[i - 1] != value))
+		return (i);
+	if (array[i] >= value)
+		return (advanced_binary_recursive(array, left, i, value));
+	return (advanced_binary_recursive(array, i + 1, right, value));
+}
+
+/**
+  * advanced_binary - Searches for a value in a sorted array
+  * @array: Array
+  * @size: The size of elements in the array.
+  * @value: The value.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  * Description: Prints the [sub]array being searched after each change.
+  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t index = 0, s = size - 1, move, r;
+	if (array == NULL || size == 0)
+		return (-1);
 
-	if (array)
-	{
-		while (index <= s)
-		{
-			printf("Searching in array: ");
-			for (r = index; r < s; r++)
-				printf("%d, ", array[r]);
-			printf("%d\n", array[r]);
-			move = (index + s) / 2;
-			if (array[move] == value)
-			{
-				if (move == 0 || array[move - 1] < value)
-					return (move);
-				s = move;
-			}
-			else if (array[move] < value)
-				index = move + 1;
-			else
-				s = move;
-		}
-	}
-	return (-1);
+	return (advanced_binary_recursive(array, 0, size - 1, value));
 }
