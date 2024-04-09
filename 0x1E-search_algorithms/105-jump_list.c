8351f863db983ed1d2053c6385172e5a5c7068cf
datlearner <kundaimuchemwa456@gmail.com>
2024-04-09 00:41:23 -0700

    Jump search in a singly linked list

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - Searches for an algorithm
+ * @list: linked list to search.
+ * @size: nodes in the list.
+ * @value: value.
+ *
+ * Return: If the value is not present or the head of the list is NULL, 
+ * Description: Prints a value every time it is compared in the list.
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t step, step_size;
+	listint_t *node, *jump;
+
+	if (list == NULL || size == 0)
+		return (NULL);
+
+	step = 0;
+	step_size = sqrt(size);
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
+	{
+		node = jump;
+		for (step += step_size; jump->index < step; jump = jump->next)
+		{
+			if (jump->index + 1 == size)
+				break;
+		}
+		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
+
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
+}
